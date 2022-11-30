1d5e156ee45b8d5abd2a610d9ded1e0e86ba10c1
JhonnyVicente1019 <jhonnyvicentecolque@gmail.com>
2022-11-30 09:57:29 -0300

    instalar-componentes

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 //importa dependencia de la biblioteca express
 import express, { json } from 'express'
 
+//importa dependencia de la biblioteca morgan
+import morgan from 'morgan'
+
+import 'dotenv/config'
+
 //crea la aplicacion 
 const app = express()
 
 //lee el body en formato json
 app.use(json())
 
+//imprime las acciones hacia cada endopoint en la terminal
+app.use(morgan('dev'))
+
 //define un puerto en que va a escuchar pedidos
-const port = 3000
+const port = process.env.PORT
 
 var playlist = [
     {
